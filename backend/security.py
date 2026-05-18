25b664cca2317340119315620a612d715d6eb6a4
Verial1 <894858@stud.unive.it>
2026-05-18 20:49:03 +0200

    Update security.py
    such that ids must be all strings

--- backend/security.py
+++ backend/security.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 SALT = os.getenv("HASH_SALT")
 
-def get_hashed_id(discord_id):
+def get_hashed_id(discord_id: str):
     payload = f"{SALT}{discord_id}"
     return hashlib.sha256(payload.encode()).hexdigest()
