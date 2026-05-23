13adf787cd42e96e31239b9e46f827024659b903
Verial1 <894858@stud.unive.it>
2026-05-23 11:49:02 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,6 @@
 from fastapi import FastAPI
-from database import engine, Base
-import models
 
 app = FastAPI(title="SNA Bot API")
-
-Base.metadata.create_all(bind=engine)
-print("Tables succesfully created!")
 
 @app.get("/")
 def root():
