a803dcd04d6379d672b8e05139f5a9b562b847a5
Verial1 <894858@stud.unive.it>
2026-05-23 20:29:33 +0200

    added comments

--- bot/bot.py
+++ bot/bot.py
@@ -7,7 +7,6 @@ import re
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-DISCORD_URL = os.getenv("DISCORD_URL")
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -19,13 +18,13 @@ async def on_ready():
     print(f'We have logged in as {client.user}')
 
 
-# DA AGGIUNGERE 'now' come keyword per forzare il delete istantaneo?
 @client.event
 async def on_message(message):
     
     if message.author == client.user:
         return
 
+    # Consent
     if message.content.startswith('/consent'):
         res = add_consent(user_id=str(message.author.id), server_id=str(message.guild.id))
         if res:                                                                                     # remember to change to ping user
@@ -33,6 +32,7 @@ async def on_message(message):
         else:
             await message.reply('Error in giving consent. Please retry.')
 
+    # Revoke
     if "/revoke" in message.content and "/revokeall" not in message.content and message.content.startswith('/revoke'):
         pattern = r'^/revoke\s+(\d{17,19})'
         match = re.search(pattern, message.content)
@@ -48,6 +48,7 @@ async def on_message(message):
         else:
             await message.reply('Error in revoking consent. Please retry.')
 
+    # Revokeall
     if "/revokeall" in message.content and message.content.startswith('/revokeall'):
         res = add_revoke(user_id=str(message.author.id), server_id="all")
         if res:
