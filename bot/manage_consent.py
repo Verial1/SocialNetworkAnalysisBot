aa918d41050d35572c6e10146d0fbf4f4c7aaab8
Verial1 <894858@stud.unive.it>
2026-05-23 11:48:04 +0200

    merged manage_consent.py and redis_comms into manage_consent.py

--- bot/manage_consent.py
+++ bot/manage_consent.py
@@ -1,35 +1,51 @@
+import json
+import time
 from security import get_hashed_id
-
-def user_consent(user_id: str, guild_id: str):
-    user_id_hashed = get_hashed_id(user_id)
-    guild_id_hashed = get_hashed_id(guild_id)
-    # check if is already saved in users (hasedId + hashedGuild)
-    # if already saved return False
-    # else add to redis queue
-    #   return True
-    return True
-
-def revoke_user_consent(user_id: str, guild_id: str):
-    user_id_hashed = get_hashed_id(user_id)
-    guild_id_hashed = get_hashed_id(guild_id)
-    # check if is saved in users (hasedId + hashedGuild)
-    # if not saved, return False
-    # else add to redis queue
-    #   return True
-    return True
-
-def revoke_user_consent_all(user_id: str):
-    user_id_hashed = get_hashed_id(user_id)
-    # check if is saved in users (hasedId)
-    # if not saved, return False
-    # else add to redis queue
-    #   return True
-    return True
+from redis_connection import r
+import redis
 
 
-def get_user_redis():
-    j = 12
-    # gets user from redis if redis user queue is not empty
-    # calls get user_data()
-    # sends data to postgresql
-    # if data is correctly saved in postgre, delete from redis cache
+def add_consent(user_id: str, server_id: str):
+    u_hash = get_hashed_id(user_id)
+    s_hash = get_hashed_id(server_id)
+    try: 
+        pipe = r.pipeline()
+        
+        pipe.sadd(f"active_consents:{u_hash}", s_hash)
+        
+        process_at = time.time() + 300
+        task_data = json.dumps({"u": user_id, "s": server_id})
+        pipe.zadd("scheduled_fetches", {task_data: process_at})
+        
+        pipe.execute()
+        return True
+    except redis.RedisError as e:
+        print(f"Redis Error in add_consent: {e}")
+        return False
+    except Exception as e:
+        print(f"General Error in add_consent: {e}")
+        return False
+
+def add_revoke(user_id: str, server_id: str):
+    u_hash = get_hashed_id(user_id)
+    s_hash = get_hashed_id(server_id)
+    try:
+        pipe = r.pipeline()
+
+        if server_id == "all":
+            pipe.delete(f"active_consents:{u_hash}")
+            pipe.lpush("revoke_queue", json.dumps({"u_hash": u_hash, "s_hash": "all"}))
+        else:
+            # active_consents is useful for tracking messages live
+            pipe.srem(f"active_consents:{u_hash}", s_hash)
+            # for postgres revoke at midnight
+            pipe.lpush("revoke_queue", json.dumps({"u_hash": u_hash, "s_hash": s_hash}))
+
+        pipe.execute()
+        return True
+    except redis.RedisError as e:
+        print(f"Redis Error in add_revoke: {e}")
+        return False
+    except Exception as e:
+        print(f"General Error in add_revoke: {e}")
+        return False
