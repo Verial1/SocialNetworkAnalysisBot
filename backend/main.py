a052f1040d4009153de53c5158b6186f7b4681ac
Verial1 <894858@stud.unive.it>
2026-05-24 18:28:05 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
+from database_connection import ENGINE, Base
+import models
 
+Base.metadata.create_all(bind=ENGINE)
 app = FastAPI(title="SNA Bot API")
 
 @app.get("/")
