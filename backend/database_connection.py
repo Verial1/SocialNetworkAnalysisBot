913e62eb0714db15902646722b6f5706cebcef0d
Verial1 <894858@stud.unive.it>
2026-05-24 18:27:53 +0200

    rename database.py in database_connection.py

--- backend/database_connection.py
+++ backend/database_connection.py
@@ -3,13 +3,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
 ENGINE = create_engine(DATABASE_URL)
 Base = declarative_base()
-
-Base.metadata.create_all(ENGINE)
-print("Tables succesfully created!")
-
 SessionLocal = sessionmaker(bind=ENGINE)
 
 def get_db():
