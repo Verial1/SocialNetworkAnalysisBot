0afac4b4e4df177d343a6af9a03fb4bf8b3c722c
Verial1 <894858@stud.unive.it>
2026-05-25 11:06:16 +0200

    create lifespan manager in backend's main.py

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,27 @@
 from fastapi import FastAPI
 from database_connection import ENGINE, Base
 import models
+from periodic_consent_check import check_scheduled
+from contextlib import asynccontextmanager
+import asyncio
+import logging
 
-Base.metadata.create_all(bind=ENGINE)
-app = FastAPI(title="SNA Bot API")
+logger = logging.getLogger('uvicorn.error')
+logger.setLevel(logging.DEBUG)
+
+# Qui defineeremoi cosa succede all'avvio e allo spegnimento
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=ENGINE)
+    logger.debug("test1")
+    asyncio.create_task(check_scheduled())
+    logger.debug("test2")
+    yield # Qui l'app rimane accesa e funzionante!!
+    
+    print("Turning off backend...")
+
+# 2. Inizializziamo l'app passandogli il lifespan
+app = FastAPI(title="SNA Bot API", lifespan=lifespan)
 
 @app.get("/")
 def root():
