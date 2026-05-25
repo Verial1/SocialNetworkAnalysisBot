d095e8e201dbeb9dec5c33f4206765d01e5c4bb2
Verial1 <894858@stud.unive.it>
2026-05-25 12:10:53 +0200

    implemented adjust_tasks()

--- backend/periodic_consent_check.py
+++ backend/periodic_consent_check.py
@@ -3,6 +3,11 @@ from redis_connection import r
 import asyncio
 import time
 import json
+from security import get_hashed_id
+#import logging
+
+#logger = logging.getLogger('uvicorn.error')
+#logger.setLevel(logging.DEBUG)
 
 # Da controllare se i dati esistono già nel database prima di mandare il fetch
 # Da controllare se c'è qualche /revoke prima e /consent dopo relativa allo stesso server
@@ -10,50 +15,98 @@ import json
 async def check_scheduled():
     while True:
         try:
+            print("Entering check_scheduled()")
             now = time.time()
-            tasks_to_fetch = r.zrangebyscore("scheduled_fetches", 0, now)
-            tasks_to_delete = r.zrangebyscore("scheduled_deletes", 0, now)
+            tasks_to_fetch = r.zrangebyscore("scheduled_fetches", 0, now, withscores=True)
+            tasks_to_delete = r.zrangebyscore("scheduled_deletes", 0, now, withscores=True)
 
             pipe = r.pipeline()
-            # da controllare se si può fare la remove by range piuttosto che singoli oggetti
-            for task_json in tasks_to_fetch:
-                pipe.zrem("scheduled_fetches", task_json)
-
-            for task_json in tasks_to_delete:            
-                pipe.zrem("scheduled_deletes", task_json)
+            pipe.zremrangebyscore("scheduled_fetches", 0, now)
+            pipe.zremrangebyscore("scheduled_deletes", 0, now)
             pipe.execute()
 
-            for task_json in tasks_to_fetch:
-                task = json.loads(task_json)
-                # anche se mi sa che lo lancerò sul bot non qui nel backend, voglio mantenere i due separati e il bot si interfaccia con i messaggi
-                # asyncio.create_task(get_message_history(task['s'], task['u']))
 
-            for task_json in tasks_to_delete:
-                task = json.loads(task_json)
-                # anche se mi sa che lo lancerò sul bot non qui nel backend, voglio mantenere i due separati e il bot si interfaccia con i messaggi
-                # asyncio.create_task(wipe_user_data(task['u'], task['s']))
+            to_adjust_fetches = []
+            to_adjust_deletes = []
 
+            for task_json, score in tasks_to_fetch:
+                task = json.loads(task_json)
+                task['timestamp'] = score 
+                to_adjust_fetches.append(task)
+
+            for task_json, score in tasks_to_delete:
+                task = json.loads(task_json)
+                task['timestamp'] = score
+                to_adjust_deletes.append(task)
+
+            fetches, deletes = await adjust_tasks(tasks_to_fetch=to_adjust_fetches, tasks_to_delete=to_adjust_deletes)
+            # chiamate a bot per fetches
+            # chiamata a db per deletes
         except Exception as e:
             print(f"Error in task runner: {e}")
 
         await asyncio.sleep(30)
 
+# ricorda, i fetch hanno id in chiaro, i delete hashati
+async def adjust_tasks(tasks_to_fetch: list, tasks_to_delete: list):
+    adjusted_fetches = []
+    adjusted_deletes = []
 
-async def adjust_tasks():
-    pass
+    db_servers = []
     # avrò una lista ordinata temporalmente di "tasks_to_fetch" aka consent e "tasks_to_delete" aka revoke
+    for delete_t in tasks_to_delete:
+        # se /revokeall
+        if delete_t['s'] == "all":
+            db_servers = [] # await get_servers_from_db(d_task['u'])    # Recupera i server dal DB Postgres
+            
+            # Controllo la lista dei fetch
+            for fetch_t in tasks_to_fetch:
+                # converto in hash per avere i corrisponendi dei delete (che sono già hashati)
+                fetch_t_u_hashed = get_hashed_id(fetch_t['u']) 
+                fetch_t_s_hashed = get_hashed_id(fetch_t['s']) 
+                # se user_id del fetch e delete corrispondono, e il timestamp di tale fetch è più recente del revokeall
+                if fetch_t_u_hashed == delete_t['u'] and fetch_t['timestamp'] > delete_t['timestamp']:
+                    # cancellalo dai revoke
+                    if fetch_t_s_hashed in db_servers:
+                        db_servers.remove(fetch_t_s_hashed)
+            
+            # aggiungi i server risultanti alla lista pulita
+            for s in db_servers:
+                adjusted_deletes.append({
+                    "u": delete_t['u'],
+                    "s": s,
+                    "timestamp": delete_t['timestamp']
+                })
+        else:
+            adjusted_deletes.append(delete_t)
 
-    # Controlla se c'è un revokeall poi un consent
-    #   SE SI: 
-    #       fetcha la lista di servers in cui è l'utente (postgres)
-    #       SE nella lista ricevuta c'è anche quella per cui c'è il consent:
-    #           elimina dalla lista ricevuta il server per cui c'è il consenso
-    #       controlla se su Redis c'è un consent incoming su uno dei server che si sta eliminando (serve perché magari il consent è stato fatto 10 secondi dopo, dopo 5 minuti e 1 secondo dal revoke c'è il fetch da redis e il consent di 9 secondi dopo se no verrebbe ignorato e ci sarebbe un fetch in più che sarebbe potuto essere risparmiato)
-    #       SE SI: toglilo dalla lista di quelli da cancellare
-    #       usa tale lista per defininire gli argomenti della chiamata wipe_user_data o quel che è
+    # ora controlla se su redis c'è qualche /consent corrispondente incoming
+    future_fetches = r.zrange("scheduled_fetches", 0, -1)
+    future_consents = set()
+    for future_task_json in future_fetches:
+                ft = json.loads(future_task_json)
+                u_h = get_hashed_id(ft['u'])
+                s_h = get_hashed_id(ft['s'])
+                future_consents.add((u_h, s_h))
 
-    # Controlla se il consent è stato già dato (fetcha la lista di servers in cui è l'utente, magari fallo ad inizio funzione che è utile pure per il revokeall)
-    #   SE SI: eliminalo dalla lista dei consent, inutile riprendere i dati
+    adjusted_deletes = [
+            d for d in adjusted_deletes 
+            if (d['u'], d['s']) not in future_consents
+        ]
+
+    
+
+    for fetch_t in tasks_to_fetch:
+        u_h = get_hashed_id(fetch_t['u'])
+        s_h = get_hashed_id(fetch_t['s'])
+
+        # Se l'utente è già nel Database per questo server, il fetch è inutile
+        # Usiamo una chiamata al backend
+        already_exists = [] # await check_if_user_exists_in_db(u_h, s_h)
+        
+        if not already_exists:
+            adjusted_fetches.append(fetch_t)
     
     # Ritorna le liste aggiustate
+    return adjusted_fetches, adjusted_deletes
     
