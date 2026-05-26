1044296c24644925789eff546cd98a446c59b161
Verial1 <894858@stud.unive.it>
2026-05-26 21:34:33 +0200

    Create init_fetch_manager.py

---
+++ bot/init_fetch_manager.py
@@ -0,0 +1,27 @@
+from redis_connection import r
+from security import get_hashed_id
+import json
+import time
+
+async def schedule_gap_fills(session_start_id: int):
+    user_keys = r.keys("active_consents:*") 
+    
+    for key in user_keys:
+        user_id = key.split(":")[1] # Estrae l'ID in chiaro, usare scan???
+        servers = r.smembers(key) 
+        
+        for server_id in servers:
+            u_hash = get_hashed_id(user_id)
+            s_hash = get_hashed_id(server_id)
+            # Chiediamo al backend dove eravamo rimasti (usando gli hash)
+            last_id = 1 #await backend_api.get_latest_id(u_hash, s_hash)
+            
+            if last_id:
+                # Programmiamo il compito di riparazione
+                task = json.dumps({
+                    "u": user_id, "s": server_id, 
+                    "type": "gap_fill", 
+                    "min_id": last_id, 
+                    "stop_id": session_start_id
+                })
+                r.zadd("scheduled_fetches", {task: time.time() + 10})
